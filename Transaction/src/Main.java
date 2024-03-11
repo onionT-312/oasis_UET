055a1029d5c4bc8955bcf87cbcc72e3d711e378e
NguyenThanh <93968586+onionT-312@users.noreply.github.com>
2024-03-11 11:23:08 +0700

    Main.java

--- Transaction/src/Main.java
+++ Transaction/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("lo con cac");
         Account acc= new Account();
         acc.addTransaction(2000.255,"deposit");
         acc.addTransaction(1000,"withdraw");
